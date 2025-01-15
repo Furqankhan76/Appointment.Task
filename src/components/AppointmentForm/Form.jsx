19c88d9f4b2e96df379ce76647ebf7319ecb29b8
furqan <furqankhanfk0999@gmail.com>
2025-01-15 14:04:19 +0530

    .

--- src/components/AppointmentForm/Form.jsx
+++ src/components/AppointmentForm/Form.jsx
@@ -136,7 +136,7 @@ function ScheduleMeetingForm() {
       const accessToken = localStorage.getItem("google_access_token");
 
       const response = await axios.post(
-        "https://www.googleapis.com/calendar/v3/calendars/primary/events?sendUpdates=all",
+        "api/calendar/v3/calendars/primary/events?sendUpdates=all",
         meetingData,
         {
           headers: {
